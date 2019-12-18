c703f922b16ccd9c243c9dfd95c028a17593b579
Revant Nandgaonkar <revant.one@gmail.com>
2019-12-18 05:33:22 +0530

    fix(rma-mobile): use staging server

--- packages/rma-mobile/src/app/api/constants/frappe-oauth2config.ts
+++ packages/rma-mobile/src/app/api/constants/frappe-oauth2config.ts
@@ -1,21 +1,21 @@
 export const OAuthProviderClientCredentials = {
   profileUrl:
-    'https://default.mntechnique.com/api/method/frappe.integrations.oauth2.openid_profile',
+    'https://staging-excelbd.castlecraft.in/api/method/frappe.integrations.oauth2.openid_profile',
 
   tokenUrl:
-    'https://default.mntechnique.com/api/method/frappe.integrations.oauth2.get_token',
+    'https://staging-excelbd.castlecraft.in/api/method/frappe.integrations.oauth2.get_token',
 
-  authServerUrl: 'https://default.mntechnique.com',
+  authServerUrl: 'https://staging-excelbd.castlecraft.in',
 
   authorizationUrl:
-    'https://default.mntechnique.com/api/method/frappe.integrations.oauth2.authorize',
+    'https://staging-excelbd.castlecraft.in/api/method/frappe.integrations.oauth2.authorize',
 
   revocationUrl:
-    'https://default.mntechnique.com/api/method/frappe.integrations.oauth2.revoke_token',
+    'https://staging-excelbd.castlecraft.in/api/method/frappe.integrations.oauth2.revoke_token',
 
   scope: 'all%20openid',
 
-  clientId: '83bdec290f',
+  clientId: '36eeb88b4f',
 
-  appUrl: 'frappe://',
+  appUrl: 'excel-rma://',
 };
