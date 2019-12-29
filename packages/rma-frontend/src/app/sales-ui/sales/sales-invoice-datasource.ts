87cbef211b6b4159db2f70bd4ecc719904051ff6
Hardik Bhanderi <bhanderi77@gmail.com>
2019-12-29 23:13:10 +0530

    feat(rma-frontend): Added Material Table for sales-invoice

---
+++ packages/rma-frontend/src/app/sales-ui/sales/sales-invoice-datasource.ts
@@ -0,0 +1,57 @@
+import { DataSource, CollectionViewer } from '@angular/cdk/collections';
+import { map, catchError, finalize } from 'rxjs/operators';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { SalesService } from '../services/sales.service';
+
+export interface ListingData {
+  uuid: string;
+  customer: string;
+  submitted: boolean;
+  total: number;
+}
+
+export interface ListResponse {
+  docs: ListingData[];
+  length: number;
+  offset: number;
+}
+export class SalesInvoiceDataSource extends DataSource<ListingData> {
+  data: ListingData[];
+  length: number;
+  offset: number;
+
+  itemSubject = new BehaviorSubject<ListingData[]>([]);
+  loadingSubject = new BehaviorSubject<boolean>(false);
+
+  loading$ = this.loadingSubject.asObservable();
+
+  constructor(private salesService: SalesService) {
+    super();
+  }
+
+  connect(collectionViewer: CollectionViewer): Observable<ListingData[]> {
+    return this.itemSubject.asObservable();
+  }
+
+  disconnect(collectionViewer: CollectionViewer): void {
+    this.itemSubject.complete();
+    this.loadingSubject.complete();
+  }
+
+  loadItems(filter = '', sortOrder = 'asc', pageIndex = 0, pageSize = 10) {
+    this.loadingSubject.next(true);
+    this.salesService
+      .getSalesInvoiceList(filter, sortOrder, pageIndex, pageSize)
+      .pipe(
+        map((res: ListResponse) => {
+          this.data = res.docs;
+          this.offset = res.offset;
+          this.length = res.length;
+          return res.docs;
+        }),
+        catchError(() => of([])),
+        finalize(() => this.loadingSubject.next(false)),
+      )
+      .subscribe(items => this.itemSubject.next(items));
+  }
+}
