e5871f3d662dbd8603c2a9446d2bbf2d4bee11af
Hardik <usamaofficial18@gmail.com>
2020-01-21 19:56:00 +0530

    feat(rma-server):fixed errors

--- packages/rma-server/src/delivery-note/entity/delivery-note-entity.module.ts
+++ packages/rma-server/src/delivery-note/entity/delivery-note-entity.module.ts
@@ -2,8 +2,15 @@ import { Module, HttpModule } from '@nestjs/common';
 import { DeliveryNoteService } from './delivery-note-service/delivery-note.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DeliveryNote } from './delivery-note-service/delivery-note.entity';
+import { SalesInvoiceEntitiesModule } from '../../sales-invoice/entity/entity.module';
+import { SerialNoEntitiesModule } from '../../serial-no/entity/entity.module';
 @Module({
-  imports: [HttpModule, TypeOrmModule.forFeature([DeliveryNote])],
+  imports: [
+    HttpModule,
+    TypeOrmModule.forFeature([DeliveryNote]),
+    SalesInvoiceEntitiesModule,
+    SerialNoEntitiesModule,
+  ],
   providers: [DeliveryNoteService],
   exports: [DeliveryNoteService],
 })
