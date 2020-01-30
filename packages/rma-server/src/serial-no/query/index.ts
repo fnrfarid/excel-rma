bb58c548048066db2554b74deb992be063a1a3ea
prafful1234 <prafful@mntechnique.com>
2020-01-30 12:32:21 +0530

    feat(rma-server): Added validation for multiple serials
    serials validation now gives a precise error returning invalid serials

--- packages/rma-server/src/serial-no/query/index.ts
+++ packages/rma-server/src/serial-no/query/index.ts
@@ -1,7 +1,9 @@
 import { RetrieveSerialNoHandler } from './get-serial-no/retrieve-serial-no-query.handler';
 import { RetrieveSerialNoListHandler } from './list-serial-no/retrieve-serial-no-list-query.handler';
+import { ValidateSerialsHandler } from './validate-serial/validate-serial-query.handler';
 
 export const SerialNoQueryManager = [
   RetrieveSerialNoHandler,
   RetrieveSerialNoListHandler,
+  ValidateSerialsHandler,
 ];
