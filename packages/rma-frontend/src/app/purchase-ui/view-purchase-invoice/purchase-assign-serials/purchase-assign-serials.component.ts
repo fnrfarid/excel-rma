b35e6416e3c99f32a44d01257691b74d1a1f2266
Hardik Bhanderi <bhanderi77@gmail.com>
2020-02-11 14:32:38 +0530

    feat(rma-server): Added serial assign part of purchase receipt

--- packages/rma-frontend/src/app/purchase-ui/view-purchase-invoice/purchase-assign-serials/purchase-assign-serials.component.ts
+++ packages/rma-frontend/src/app/purchase-ui/view-purchase-invoice/purchase-assign-serials/purchase-assign-serials.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { PurchaseInvoiceDetails } from '../../../common/interfaces/purchase.interface';
 import { PurchaseService } from '../../services/purchase.service';
 import { ActivatedRoute } from '@angular/router';
-import { MatSnackBar } from '@angular/material';
-import { startWith, switchMap } from 'rxjs/operators';
+import { MatSnackBar, MatDialog } from '@angular/material';
+import {
+  startWith,
+  switchMap,
+  debounceTime,
+  distinctUntilChanged,
+} from 'rxjs/operators';
 import { SalesService } from '../../../sales-ui/services/sales.service';
 import { CLOSE } from '../../../constants/app-string';
 import { ERROR_FETCHING_PURCHASE_INVOICE } from '../../../constants/messages';
@@ -15,6 +20,13 @@ import {
 } from '../../../common/interfaces/purchase-receipt.interface';
 import { Location } from '@angular/common';
 import { LoadingController } from '@ionic/angular';
+import {
+  ItemDataSource,
+  SerialDataSource,
+} from '../../../sales-ui/view-sales-invoice/serials/serials-datasource';
+import * as _ from 'lodash';
+import { Item } from 'src/app/common/interfaces/sales.interface';
+import { AssignSerialsDialog } from 'src/app/sales-ui/view-sales-invoice/serials/serials.component';
 
 @Component({
   selector: 'purchase-assign-serials',
@@ -23,16 +35,9 @@ import { LoadingController } from '@ionic/angular';
 })
 export class PurchaseAssignSerialsComponent implements OnInit {
   warehouseFormControl = new FormControl('', [Validators.required]);
-  displayedColumns: string[] = [
-    'position',
-    'serial',
-    'item',
-    'company',
-    'supplier',
-    'claimsReceivedDate',
-    'clear',
-  ];
   dataSource = [];
+  csvFile: any;
+  value: string;
   date = new FormControl(new Date());
   purchaseReceiptDate: string;
 
@@ -40,16 +45,49 @@ export class PurchaseAssignSerialsComponent implements OnInit {
   purchaseInvoiceDetails: PurchaseInvoiceDetails;
   getOptionText = '';
 
+  rangePickerState = {
+    prefix: '',
+    fromRange: 0,
+    toRange: 0,
+    serials: [],
+  };
+
+  fromRangeUpdate = new Subject<string>();
+  toRangeUpdate = new Subject<string>();
+  itemDisplayedColumns = [
+    'item_code',
+    'item_name',
+    'qty',
+    'assigned',
+    'remaining',
+    'add_serial',
+  ];
+  itemDataSource: ItemDataSource;
+  serialDisplayedColumns = [
+    'item_code',
+    'item_name',
+    'qty',
+    // 'warranty_date',
+    'serial_no',
+    'delete',
+  ];
+  serialDataSource: SerialDataSource;
+
   constructor(
-    private readonly snackbar: MatSnackBar,
+    private readonly snackBar: MatSnackBar,
     private readonly route: ActivatedRoute,
     private readonly purchaseService: PurchaseService,
     private readonly location: Location,
     private readonly salesService: SalesService,
+    public dialog: MatDialog,
     private loadingController: LoadingController,
   ) {}
 
   ngOnInit() {
+    this.onFromRange(this.value);
+    this.onToRange(this.value);
+    this.serialDataSource = new SerialDataSource();
+    this.itemDataSource = new ItemDataSource();
     this.purchaseReceiptDate = this.getParsedDate(this.date.value);
     this.getPuchaseInvoice(this.route.snapshot.params.invoiceUuid);
     this.purchaseInvoiceDetails = {} as PurchaseInvoiceDetails;
@@ -65,27 +103,16 @@ export class PurchaseAssignSerialsComponent implements OnInit {
     this.purchaseService.getPurchaseInvoice(uuid).subscribe({
       next: (res: PurchaseInvoiceDetails) => {
         this.purchaseInvoiceDetails = res;
-        const itemList = [];
-        res.items.forEach(item => {
-          for (let j = 0; j < item.qty; j++) {
-            itemList.push({
-              position: j + 1,
-              serial_no: '',
-              item_name: item.item_name,
-              company: res.company,
-              claimsReceivedDate: this.purchaseReceiptDate,
-              rate: item.rate,
-              qty: 1,
-              amount: item.rate,
-              item_code: item.item_code,
-              supplier: res.supplier,
-            });
-          }
-          this.dataSource = itemList;
-        });
+        this.itemDataSource.loadItems(
+          res.items.filter(item => {
+            item.assigned = 0;
+            item.remaining = item.qty;
+            return item;
+          }),
+        );
       },
       error: err => {
-        this.snackbar.open(
+        this.snackBar.open(
           err.error.message
             ? err.error.message
             : `${ERROR_FETCHING_PURCHASE_INVOICE}${err.error.error}`,
@@ -96,7 +123,7 @@ export class PurchaseAssignSerialsComponent implements OnInit {
     });
   }
 
-  async submitDeliveryNote() {
+  async submitPurchaseReceipt() {
     const loading = await this.loadingController.create({
       message: 'Creating Serials',
       spinner: 'bubbles',
@@ -115,8 +142,9 @@ export class PurchaseAssignSerialsComponent implements OnInit {
     purchaseReceipt.items = [];
 
     const filteredItemCodeList = [
-      ...new Set(this.dataSource.map(item => item.item_code)),
+      ...new Set(this.serialDataSource.data().map(item => item.item_code)),
     ];
+
     filteredItemCodeList.forEach(item_code => {
       const purchaseReceiptItem = {} as PurchaseReceiptItem;
       purchaseReceiptItem.warehouse = this.warehouseFormControl.value;
@@ -125,11 +153,14 @@ export class PurchaseAssignSerialsComponent implements OnInit {
       purchaseReceiptItem.amount = 0;
       purchaseReceiptItem.rate = 0;
       purchaseReceiptItem.item_code = item_code;
-      this.dataSource.forEach(item => {
-        if (item_code === item.item_code && item.serial_no !== '') {
-          purchaseReceiptItem.qty += 1;
-          purchaseReceiptItem.amount += item.rate;
-          purchaseReceiptItem.serial_no.push(item.serial_no);
+
+      this.serialDataSource.data().forEach(item => {
+        if (item_code === item.item_code && item.serial_no.length !== 0) {
+          purchaseReceiptItem.qty += item.qty;
+          purchaseReceiptItem.amount += item.rate * item.qty;
+          item.serial_no.forEach(serial => {
+            purchaseReceiptItem.serial_no.push(serial);
+          });
           purchaseReceiptItem.rate = item.rate;
           purchaseReceiptItem.item_name = item.item_name;
         }
@@ -142,18 +173,210 @@ export class PurchaseAssignSerialsComponent implements OnInit {
     this.purchaseService.createPurchaseReceipt(purchaseReceipt).subscribe({
       next: success => {
         loading.dismiss();
-        this.snackbar.open('Purchase Receipt created', CLOSE, {
+        this.snackBar.open('Purchase Receipt created', CLOSE, {
           duration: 2500,
         });
         this.location.back();
       },
       error: err => {
         loading.dismiss();
-        this.snackbar.open('Something went wrong ', CLOSE, {
+        this.snackBar.open('Something went wrong ', CLOSE, {
           duration: 2500,
         });
       },
     });
+  }
+
+  onFromRange(value) {
+    this.fromRangeUpdate
+      .pipe(debounceTime(400), distinctUntilChanged())
+      .subscribe(v => {
+        this.generateSerials(value);
+      });
+  }
+
+  onToRange(value) {
+    this.toRangeUpdate
+      .pipe(debounceTime(400), distinctUntilChanged())
+      .subscribe(v => {
+        this.generateSerials(undefined, value);
+      });
+  }
+
+  onPrefixChange(value) {
+    this.generateSerials(undefined, undefined, value);
+  }
+
+  async generateSerials(fromRange?, toRange?, prefix?) {
+    this.rangePickerState.serials =
+      (await this.getSerialsFromRange(
+        fromRange || this.rangePickerState.fromRange || 0,
+        toRange || this.rangePickerState.toRange || 0,
+        prefix || this.rangePickerState.prefix,
+      )) || [];
+  }
+
+  getSerialsFromRange(start: number, end: number, prefix: string) {
+    const data: any[] = _.range(
+      start > end ? Number(start) + 1 : start,
+      end > start ? Number(end) + 1 : end,
+    );
+    const maxSerial =
+      start.toString().length > end.toString().length
+        ? start.toString().length
+        : end.toString().length;
+    for (let i = 0; i < data.length; i++) {
+      data[i] = `${prefix}${this.getPaddedNumber(data[i], maxSerial)}`;
+    }
+    return data;
+  }
+
+  getPaddedNumber(num, numberLength) {
+    return _.padStart(num, numberLength, '0');
+  }
+
+  async assignSingularSerials(row: Item) {
+    const dialogRef =
+      row.remaining >= 30
+        ? this.dialog.open(AssignSerialsDialog, {
+            width: '250px',
+            data: { serials: row.remaining || 0 },
+          })
+        : null;
+
+    const serials =
+      row.remaining >= 30
+        ? await dialogRef.afterClosed().toPromise()
+        : row.remaining;
+    if (serials) {
+      this.addSingularSerials(row, serials);
+      this.resetRangeState();
+      this.updateProductState(row, serials);
+    }
+  }
+
+  assignRangeSerial(row: Item) {
+    const data = this.serialDataSource.data();
+    data.push({
+      item_code: row.item_code,
+      item_name: row.item_name,
+      qty: this.rangePickerState.serials.length,
+      rate: row.rate,
+      amount: row.amount,
+      serial_no: this.rangePickerState.serials,
+    });
+    this.updateProductState(
+      row.item_code,
+      this.rangePickerState.serials.length,
+    );
+    this.serialDataSource.update(data);
+    this.resetRangeState();
+  }
+
+  assignSerial(itemRow: Item) {
+    if (!this.rangePickerState.serials.length) {
+      this.assignSingularSerials(itemRow);
+      return;
+    }
+
+    if (itemRow.remaining < this.rangePickerState.serials.length) {
+      this.snackBar.open(
+        `Only ${itemRow.remaining} serials could be assigned to ${itemRow.item_code}`,
+        CLOSE,
+        { duration: 2500 },
+      );
+      return;
+    }
+    this.validateSerial(
+      { item_code: itemRow.item_code, serials: this.rangePickerState.serials },
+      itemRow,
+    );
+  }
+
+  validateSerial(item: { item_code: string; serials: string[] }, row: Item) {
+    this.salesService.validateSerials(item).subscribe({
+      next: (success: { notFoundSerials: string[] }) => {
+        success.notFoundSerials &&
+        success.notFoundSerials.length === item.serials.length
+          ? this.assignRangeSerial(row)
+          : this.snackBar.open(
+              `Invalid Serials ${this.getInvalidSerials(
+                item.serials,
+                success.notFoundSerials,
+              )
+                .splice(0, 5)
+                .join(', ')}...`,
+              CLOSE,
+              { duration: 2500 },
+            );
+      },
+      error: err => {},
+    });
+  }
+
+  getInvalidSerials(arr1, arr2) {
+    return _.difference(arr1, arr2);
+  }
+
+  addSingularSerials(row, serialCount) {
+    this.updateProductState(row.item_code, serialCount);
+    const serials = this.serialDataSource.data();
+    Array.from({ length: serialCount }, (x, i) => {
+      serials.push({
+        item_code: row.item_code,
+        item_name: row.item_name,
+        qty: 1,
+        rate: row.rate,
+        amount: row.amount,
+        serial_no: [''],
+      });
+    });
+    this.serialDataSource.update(serials);
+  }
+
+  updateProductState(item_code, assigned) {
+    const itemState = this.itemDataSource.data();
+    itemState.filter(product => {
+      if (product.item_code === item_code) {
+        product.assigned = product.assigned + assigned;
+        product.remaining = product.qty - product.assigned;
+      }
+      return product;
+    });
+    this.itemDataSource.update(itemState);
+  }
+
+  deleteRow(row, i) {
+    let serialData = this.serialDataSource.data();
+    serialData.length === 1 ? (serialData = []) : serialData.splice(i, 1);
+
+    this.serialDataSource.update(serialData);
+    let itemData = this.itemDataSource.data();
+
+    itemData = itemData.filter(item => {
+      if (item.item_code === row.item_code) {
+        item.assigned = item.assigned - row.qty;
+        item.remaining = item.remaining + row.qty;
+      }
+      return item;
+    });
+
+    this.itemDataSource.update(itemData);
+  }
+
+  getSerialsInputValue(row) {
+    return row.serial_no.length === 1
+      ? row.serial_no[0]
+      : `${row.serial_no[0]} - ${row.serial_no[row.serial_no.length - 1]}`;
+  }
+
+  resetRangeState() {
+    this.rangePickerState = {
+      prefix: '',
+      fromRange: 0,
+      toRange: 0,
+      serials: [],
+    };
   }
 
   updateSerial(element, serial_no) {
@@ -163,7 +386,7 @@ export class PurchaseAssignSerialsComponent implements OnInit {
       this.salesService.getSerial(serial_no).subscribe({
         next: res => {
           this.dataSource[index].serial_no = '';
-          this.snackbar.open('Serial No already in use.', CLOSE, {
+          this.snackBar.open('Serial No already in use.', CLOSE, {
             duration: 2500,
           });
         },
@@ -198,5 +421,13 @@ export class PurchaseAssignSerialsComponent implements OnInit {
       // +1 as index of months start's from 0
       date.getDate(),
     ].join('-');
+  }
+
+  fileChangedEvent($event): void {
+    const reader = new FileReader();
+    reader.readAsText($event.target.files[0]);
+    reader.onload = (file: any) => {
+      this.csvFile = file.target.result;
+    };
   }
 }
