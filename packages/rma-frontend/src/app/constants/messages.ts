d0e17c8116c7d20a24aa246ead0cbf042802e577
Revant Nandgaonkar <revant.one@gmail.com>
2020-02-11 22:44:55 +0530

    feat(rma-frontend): validate stock before add/edit SI

--- packages/rma-frontend/src/app/constants/messages.ts
+++ packages/rma-frontend/src/app/constants/messages.ts
@@ -4,3 +4,4 @@ export const ERROR_FETCHING_SALES_INVOICE =
 export const SERIAL_ASSIGNED = 'Serials successfully assigned ';
 export const ERROR_FETCHING_PURCHASE_INVOICE =
   'Error fetching Purchase Invoice - Code ';
+export const INSUFFICIENT_STOCK_BALANCE = 'Insufficient stock';
