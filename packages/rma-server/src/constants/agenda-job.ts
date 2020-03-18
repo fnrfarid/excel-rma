e7f2f4f6a162c78217e61fe4e256a8524ebebf95
prafful1234 <prafful@mntechnique.com>
2020-03-18 17:31:40 +0530

    feat(rma-server): Added agenda sync for purchase receipt

---
+++ packages/rma-server/src/constants/agenda-job.ts
@@ -0,0 +1,15 @@
+export const AGENDA_JOB_METADATA = {
+  name: '',
+  type: 'single',
+  data: null,
+  lastModifiedBy: null,
+  nextRunAt: new Date(),
+  token: null,
+  priority: 0,
+  inQueue: false,
+  repeatInterval: null,
+  repeatTimezone: null,
+  lockedAt: null,
+  lastRunAt: new Date(),
+  lastFinishedAt: new Date(),
+};
