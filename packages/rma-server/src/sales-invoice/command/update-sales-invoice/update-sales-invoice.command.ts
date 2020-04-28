26f1bc0e2e90df2592894e6a4705387df1de0c8c
Hardik Bhanderi <bhanderi77@gmail.com>
2020-04-28 21:59:12 +0530

    fix(rma-server): Fixed Sales Invoice Territory changing to All Territories when editing the Sales Invoice

--- packages/rma-server/src/sales-invoice/command/update-sales-invoice/update-sales-invoice.command.ts
+++ packages/rma-server/src/sales-invoice/command/update-sales-invoice/update-sales-invoice.command.ts
@@ -2,5 +2,8 @@ import { ICommand } from '@nestjs/cqrs';
 import { SalesInvoiceUpdateDto } from '../../entity/sales-invoice/sales-invoice-update-dto';
 
 export class UpdateSalesInvoiceCommand implements ICommand {
-  constructor(public readonly updatePayload: SalesInvoiceUpdateDto) {}
+  constructor(
+    public readonly updatePayload: SalesInvoiceUpdateDto,
+    public readonly clientHttpRequest: any,
+  ) {}
 }
