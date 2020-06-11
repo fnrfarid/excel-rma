d88fe6a0ba74c80755999fa52367e0186d907928
Hardik <usamaofficial18@gmail.com>
2020-06-11 14:09:58 +0530

    feat(rma-frontend):Added create service invoice fields

--- packages/rma-frontend/src/app/warranty-ui/view-warranty-claims/service-invoices/add-service-invoice/add-service-invoice.page.ts
+++ packages/rma-frontend/src/app/warranty-ui/view-warranty-claims/service-invoices/add-service-invoice/add-service-invoice.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-service-invoice',
@@ -7,11 +8,49 @@ import { Location } from '@angular/common';
   styleUrls: ['./add-service-invoice.page.scss'],
 })
 export class AddServiceInvoicePage implements OnInit {
+  postingDate: string;
+  serviceInvoiceForm: FormGroup;
+  get f() {
+    return this.serviceInvoiceForm.controls;
+  }
   constructor(private readonly location: Location) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.createFormGroup();
+  }
+  createFormGroup() {
+    this.serviceInvoiceForm = new FormGroup({
+      warehouse: new FormControl('', [Validators.required]),
+      customerName: new FormControl('', [Validators.required]),
+      customerContact: new FormControl('', [Validators.required]),
+      customerAddress: new FormControl('', [Validators.required]),
+      thirdPartyName: new FormControl('', [Validators.required]),
+      thirdPartyContact: new FormControl('', [Validators.required]),
+      thirdPartyAddress: new FormControl('', [Validators.required]),
+      date: new FormControl('', [Validators.required]),
+      account: new FormControl('', [Validators.required]),
+      postingDate: new FormControl('', [Validators.required]),
+      branch: new FormControl('', [Validators.required]),
+    });
+  }
 
   navigateBack() {
     this.location.back();
   }
+
+  selectedPostingDate($event) {
+    this.postingDate = this.getParsedDate($event.value);
+  }
+
+  getParsedDate(value) {
+    const date = new Date(value);
+    return [
+      date.getFullYear(),
+      date.getMonth() + 1,
+      // +1 as index of months start's from 0
+      date.getDate(),
+    ].join('-');
+  }
+
+  submitDraft() {}
 }
