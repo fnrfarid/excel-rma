f50d8b16b3b599a154dbc24522b96a9479787a43
Rehan Hash <hash4h@gmail.com>
2021-05-02 13:52:13 +0530

    feat(rma-server): pipeline test fail fixed.

--- packages/rma-server/src/item/entity/item/item.service.ts
+++ packages/rma-server/src/item/entity/item/item.service.ts
@@ -82,7 +82,7 @@ export class ItemService {
         if (['bundle_items', 'is_stock_item'].includes(key)) {
           return;
         }
-        if(['item_name'].includes(key)){
+        if (['item_name'].includes(key)) {
           return;
         }
         if (key === 'status' && query[key] === 'All') {
